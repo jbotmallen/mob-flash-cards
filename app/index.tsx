3313cf18b98787e4c6f938ebef7498594aa1db87
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2024-09-12 21:31:17 +0800

    mob-flash-cards: added carousel and buttons for switching

---
+++ app/index.tsx
@@ -0,0 +1,122 @@
+import { Content } from '@/constants/Content';
+import { useRef, useState } from 'react';
+import { Dimensions, Image, Pressable, Text, View } from 'react-native';
+import Carousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
+import Feather from '@expo/vector-icons/Feather';
+
+function Index() {
+  const [index, setIndex] = useState<number>(0);
+  const width = Dimensions.get('window').width - 20;
+  const height = Dimensions.get('window').height - 350;
+
+  const carouselRef = useRef<ICarouselInstance>(null);
+
+  const handlePrevious = () => {
+    if (index > 0) {
+      setIndex((prev) => prev - 1);
+      if (carouselRef.current) {
+        carouselRef.current.scrollTo({ index: index - 1, animated: true });
+      }
+    }
+  };
+
+  const handleNext = () => {
+    if (index < Content.length - 1) {
+      setIndex((prev) => prev + 1);
+      if (carouselRef.current) {
+        carouselRef.current.scrollTo({ index: index + 1, animated: true });
+      }
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-evenly", padding: 24, gap: 20 }}>
+      <Carousel
+        ref={carouselRef}
+        loop={false}
+        width={width}
+        height={height}
+        autoPlay={false}
+        data={Content}
+        scrollAnimationDuration={1000}
+        onSnapToItem={(index) => setIndex(index)}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flex: 1,
+              width: "100%",
+              borderWidth: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 10,
+              gap: 10,
+              padding: 10,
+              position: 'relative',
+            }}
+          >
+            <Image
+              style={{ width: "100%", height: "100%", borderRadius: 0, position: "absolute", }}
+              source={{
+                uri: item.qImage,
+              }}
+            />
+            <Text style={{ textAlign: 'center', fontSize: 30, fontWeight: '800', padding: 10, backgroundColor: "white", borderRadius: 20 }}>
+              {item.question}
+            </Text>
+          </View>
+        )}
+      />
+      <Text style={{ fontSize: 24, textAlign: "center", fontWeight: 400 }}>
+        Learn more about the
+        <Text style={{ fontWeight: 800 }}> capital cities </Text>
+        of the world!
+      </Text>
+      <View style={{ height: "10%", display: "flex", flexDirection: "row", gap: 10, marginTop: 10, justifyContent: "space-between" }}>
+        <Pressable
+          style={index === 0 ? {
+            width: "50%",
+            padding: 10,
+            backgroundColor: "gray",
+            borderRadius: 10,
+            alignItems: "center",
+            justifyContent: "center",
+          } : {
+            width: "50%",
+            padding: 10,
+            backgroundColor: "blue",
+            borderRadius: 10,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={handlePrevious}
+        >
+          <Feather name="chevrons-left" size={40} color="white" />
+        </Pressable>
+        <Pressable
+          disabled={index === Content.length - 1}
+          style={Content.length - 1 === index ?
+            {
+              width: "50%",
+              padding: 10,
+              backgroundColor: "gray",
+              borderRadius: 10,
+              alignItems: "center",
+              justifyContent: "center",
+            } : {
+              width: "50%",
+              padding: 10,
+              backgroundColor: "blue",
+              borderRadius: 10,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          onPress={handleNext}
+        >
+          <Feather name="chevrons-right" size={40} color="white" />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+export default Index;
---
+++ constants/Content.ts
@@ -0,0 +1,32 @@
+export const Content = [
+    {
+        question: "What is the capital of France?",
+        qImage: "https://plus.unsplash.com/premium_photo-1675367606971-363cda30578c?q=80&w=1846&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        answer: "Paris"
+    },
+    {
+        question: "What is the capital of Germany?",
+        qImage: "https://plus.unsplash.com/premium_photo-1674590091046-18f2ad87f344?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        answer: "Berlin"
+    },
+    {
+        question: "What is the capital of Italy?",
+        qImage: "https://plus.unsplash.com/premium_photo-1674591172725-26cce04011ff?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        answer: "Rome"
+    },
+    {
+        question: "What is the capital of Spain?",
+        qImage: "https://plus.unsplash.com/premium_photo-1674588218567-120937a97b7e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        answer: "Madrid"
+    },
+    {
+        question: "What is the capital of the United Kingdom?",
+        qImage: "https://plus.unsplash.com/premium_photo-1674591172569-834e3c928c3d?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        answer: "London"
+    },
+    {
+        question: "What is the capital of the United States?",
+        qImage: "https://plus.unsplash.com/premium_photo-1674591172747-2c1d461d7b68?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        answer: "Washington D.C."
+    }
+]
